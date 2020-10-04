0d843039d85c61da43f802b30b4bfa1f9505f7fc
Amit Kumar <amitnitsri25@gmail.com>
2020-10-05 01:19:49 +0530

    master

---
+++ src/main/java/com/maximum/Maximum.java
@@ -0,0 +1,9 @@
+
+package com.maximum;
+public class Maximum {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Maximum Program ");
+	}
+
+}
---
+++ src/test/java/com/maximum/MaximumTest.java
@@ -0,0 +1,12 @@
+
+package com.maximum;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MaximumTest {	
+	
+
+}
