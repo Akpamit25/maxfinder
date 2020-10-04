4aefec5fb9dc6ae515d0f4b926e4a82d78f983a0
Amit Kumar <amitnitsri25@gmail.com>
2020-10-05 01:36:41 +0530

    UC1

--- src/main/java/com/maximum/Maximum.java
+++ src/main/java/com/maximum/Maximum.java
@@ -1,9 +1,18 @@
 
 package com.maximum;
 public class Maximum {
-	
-	public static void main(String[] args) {
-		System.out.println("Welcome to Maximum Program ");
+		
+	public Integer findMaximumInteger(Integer[] integers) {
+		
+		Integer maxInt=integers[0];
+		if(integers[1].compareTo(maxInt)>0)
+			maxInt=integers[1];
+		if(integers[2].compareTo(maxInt)>0)
+			maxInt=integers[2];
+		System.out.println(maxInt);
+		
+		return maxInt;	
+		
 	}
 
 }
--- src/test/java/com/maximum/MaximumTest.java
+++ src/test/java/com/maximum/MaximumTest.java
@@ -1,12 +1,33 @@
 
 package com.maximum;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.Assert;
 
-public class MaximumTest {	
-	
+public class MaximumTest {
+
+	Maximum max;
+
+	@Before
+	public void initialize() {
+		max = new Maximum();
+	}
+
+	@Test
+	public void givenMaxNumber_At1stPosition_ReturnNumber() {
+		Assert.assertEquals(new Integer(3), max.findMaximumInteger(new Integer[] {3,2,1}));
+		
+	}	
+	@Test
+	public void givenMaxNumber_At2ndPosition_ReturnNumber() {
+		Assert.assertEquals(new Integer(4), max.findMaximumInteger(new Integer[] {3,4,1}));
+		
+	}
+	@Test
+	public void givenMaxNumber_At3rdPosition_ReturnNumber() {
+		Assert.assertEquals(new Integer(5), max.findMaximumInteger(new Integer[] {3,4,5}));
+		
+	}
 
 }
